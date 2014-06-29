5e8944f79bc2cec05a5682ff2a9d60ae7d7678dc
WORMSS <github@wormss.net>
2014-06-29 17:59:00 +0100

    Added Resources Constants.

---
+++ src/main/java/net/wormss/letsmodreboot/resources/R.java
@@ -0,0 +1,21 @@
+package net.wormss.letsmodreboot.resources;
+
+public final class R {
+	private static final String PACKAGE = "net.wormss.letsmodreboot";
+	public static final class mod {
+		public static final String ID = "LetsModReboot";
+		public static final String NAME = "Lets Mod Reboot";
+		public static final String VERSION = "1.7.2-1.0";
+	}
+
+	public static final class proxy {
+		public static final String CLIENT = PACKAGE + ".proxy.ClientProxy";
+		public static final String SERVER = PACKAGE + ".proxy.ServerProxy";
+	}
+
+	public static final class config {
+		public static final String VALUE = "configValue";
+	}
+}
+
+	
